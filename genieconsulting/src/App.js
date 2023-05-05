ed2abe2e213fc7932407059883697d0bd9d4062d
sebi popescu <Dogelover5381@gmail.com>
2023-05-05 11:31:50 -0400

    added frontend draft and input handling, ready for backend

--- genieconsulting/src/App.js
+++ genieconsulting/src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  //store user input
+  const [input, updateInput] = useState();
+
+  //store flask answer
+  const [answer, updateAnswer] = useState();
+
+  const sendInput = async (event) => {
+    if (event.key != "Enter") {
+      return;
+    }
+    //send the input to the flask backend
+    if (input != '') {  
+      console.log('Input: ', input);
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="chat-area">
+            <input
+              type="text"
+              className="chat-input"
+              placeholder="Input Question Here"
+              onChange={(e) => updateInput(e.target.value)}
+              onKeyDown={(e) => sendInput(e)}
+          />
+          <div className="chat-answer">
+            <b>GenieConsultant:</b> answer answer answer answer answer answer
+          </div>
+        </div>
       </header>
     </div>
   );
